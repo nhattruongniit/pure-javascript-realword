11c7766e398b90dcc8799994f22d627f34a788f8
nhattruongniit <nhattruongniit@yahoo.com.vn>
2018-03-31 12:19:35 +0700

    finished

--- js/main.js
+++ js/main.js
@@ -2,43 +2,24 @@ window.onload = function() {
     fetchIssues();
 
     document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+};
 
-}
-
-var id = 1;
+var id = 0;
+var issues = JSON.parse(localStorage.getItem('issues'));
 
 function fetchIssues() {
-    // var issues = [{
-    //         id: '1',
-    //         desc: 'description',
-    //         severity: 'hight',
-    //         assignedTo: 'Truongg',
-    //         status: 'On'
-    //     },
-    //     {
-    //         id: '2',
-    //         desc: 'description',
-    //         severity: 'hight',
-    //         assignedTo: 'Truongg',
-    //         status: 'On'
-    //     }
-    // ];
-    var issues = JSON.parse(localStorage.getItem('issues'));
     var issueList = document.getElementById('issuesList');
 
     if (issues == null) return false;
 
     issueList.innerHTML = "";
 
-
     for (var i = 0; i < issues.length; i++) {
-        console.log(issues);
         var id = issues[i].id;
         var desc = issues[i].description;
         var severity = issues[i].severity;
         var assignedTo = issues[i].assignedTo;
         var status = issues[i].status;
-
 
         issueList.innerHTML += '<div class="well">' +
             '<h6>Issue ID: ' + id + '</h6>' +
@@ -50,18 +31,57 @@ function fetchIssues() {
             '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' + id + '\')">Delete</a>' +
             '</div>';
     };
-
-
 };
 
 function saveIssue(e) {
-    var issueId = id++;
+    var issueId = ++id;
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueStatus = 'Open';
 
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    };
 
+    if (localStorage.getItem('issues') === null) {
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
 
     document.getElementById('issueInputForm').reset();
 
     fetchIssues();
-
     e.preventDefault();
+}
+
+function setStatusClosed(id) {
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues[i].status = "Closed";
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
+}
+
+function deleteIssue(id) {
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
 }
